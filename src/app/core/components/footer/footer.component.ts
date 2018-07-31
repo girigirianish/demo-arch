cc3a62f1fc71d85fffef54bcbf3d6d7e25856a7b
giri.girianish <giri.girianish@gmail.com>
2018-07-31 17:50:36 +0545

    added favicon and fixed footer issue

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,16 +1,16 @@
-import { Component, Input, OnChanges } from "@angular/core";
-import { FooterModel } from "./footer.model";
+import { Component, Input, OnChanges } from '@angular/core';
+import { FooterModel } from './footer.model';
 
 @Component({
-  selector: "app-footer",
-  templateUrl: "./footer.component.pug",
-  styleUrls: ["./footer.component.scss"]
+  selector: 'app-footer',
+  templateUrl: './footer.component.pug',
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnChanges {
   @Input() portalConfig: Object;
   @Input() portalVersion: String;
-  footerViewModel: FooterModel;
-  public footerImage: any = "../../../assets/img/powered-by-virmedica.png";
+  footerViewModel: FooterModel = new FooterModel('', '', []);
+  public footerImage: any = '../../../assets/img/powered-by-virmedica.png';
   constructor() {}
 
   ngOnChanges(changes: any): void {
